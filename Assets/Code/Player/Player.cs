fafb7d2fef476fe61e9658cab754a02eaa7a7507
Alejandro Estévez <estevezmartinez.alejandro@gmail.com>
2023-10-06 17:36:42 +0200

    Add unity components to player

--- Assets/Code/Player/Player.cs
+++ Assets/Code/Player/Player.cs
@@ -5,18 +5,6 @@ namespace Code.Player
 {
     public class Player : MonoBehaviour, IInputs
     {
-        // Start is called before the first frame update
-        void Start()
-        {
-        
-        }
-
-        // Update is called once per frame
-        void Update()
-        {
-        
-        }
-
         public void ActionButtonPressed()
         {
             Debug.Log("action pressed");
