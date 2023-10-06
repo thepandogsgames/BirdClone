fdbfca3b1406d8266003a52e7b4528883e42845b
Alejandro Estévez <estevezmartinez.alejandro@gmail.com>
2023-10-06 17:33:04 +0200

    Create player and reacts to input

---
+++ Assets/Code/Player/Player.cs
@@ -0,0 +1,25 @@
+using Code.Input;
+using UnityEngine;
+
+namespace Code.Player
+{
+    public class Player : MonoBehaviour, IInputs
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+        
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        
+        }
+
+        public void ActionButtonPressed()
+        {
+            Debug.Log("action pressed");
+        }
+    }
+}
